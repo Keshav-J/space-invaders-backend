6f9e578b8e515c43a944b8ecfd0d0f9fd6a5ed37
Keshav J <jkeshav15@gmail.com>
2020-06-22 01:34:48 +0530

    iniital backend

--- index.js
+++ index.js
@@ -1,19 +1,83 @@
 const express = require('express');
-
 const app = express();
 
-app.get("/", function(req, res) {
-    // console.log(req);
-    var scores = [
-        {name: 'Ananth', score: 100},
-        {name: 'Kalki', score: 99},
-        {name: 'Yedhu', score: 98},
-        {name: 'Krishnan', score: 97}
-    ]
-    res.send(scores);
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://keshav:FMoyhiGQmakj5Npy@space-invaders-0xdmm.mongodb.net/space-invaders', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const scoreSchema = new mongoose.Schema({
+    _id: Number,
+    name: String,
+    score: Number
+});
+
+const Score = mongoose.model('score', scoreSchema);
+
+app.get("/getScores/", function(req, res) {
+
+    Score.find(function(err, scores) {
+        if(err) {
+            res.send('err');
+        }
+        else {
+            res.send(scores);
+        }
+    });
+
+});
+
+app.get("/setScores/", function(req, res) {
+    console.log(req.query);
+    
+    const curName = req.query.name;
+    const curScore = req.query.score;
+    
+    Score.find(function(err, scores) {
+        if(err) {
+            res.send('err');
+        }
+        else {
+            Score.remove({}).then(function() {
+                if(scores.length != 0) {
+                    var flag = false;
+                    for(var i=0 ; i<scores.length ; ++i) {
+                        if(curScore >= scores[i].score) {
+                            scores.splice(i, 0, {
+                                _id  : i+1,
+                                name : curName,
+                                score: curScore
+                            });
+                            flag = true;
+                            break;
+                        }
+                    }
+                    if(scores.length > 10)
+                        scores.pop();
+                    if(!flag && scores.length<10) {
+                        scores.push({
+                            _id  : i+1,
+                            name: curName,
+                            score: curScore
+                        });
+                    }
+                }
+                else {
+                    scores.push({
+                        _id  : 1,
+                        name: curName,
+                        score: curScore
+                    });
+                }
+            
+                for(i=0 ; i<scores.length ; ++i)
+                    scores[i]._id = i+1;
+
+                Score.insertMany(scores).then(res.send('req.body'));
+            });
+        }
+    });
 });
 
 
-app.listen(process.env.PORT, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log('Server started at port: 3000');
 });
