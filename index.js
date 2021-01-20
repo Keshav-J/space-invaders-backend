8a311ad416a8554f8bee0235bf3869a5aa82938f
Keshav J <jkeshav15@gmail.com>
2021-01-21 02:54:47 +0530

    REST architecture with CORS management

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const jsonParser = bodyParser.json();
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://keshav:FMoyhiGQmakj5Npy@space-invaders-0xdmm.mongodb.net/space-invaders', { useNewUrlParser: true, useUnifiedTopology: true });
 
+const cors = require('cors');
+
 const redis = require('redis');
 const redisSpan = 120;
 const redisConfig = {
@@ -27,6 +29,8 @@ const Score = mongoose.model('score', scoreSchema);
 const PORT = process.env.PORT || 3000;
 
 function getScores(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
     client.get('scores', (err, data) => {
         if (err) {
             res.send('err');
@@ -47,44 +51,59 @@ function getScores(req, res) {
 
 function setScores(req, res) {
     const curName = req.body.name;
-    const curScore = req.body.score;
-    
+    const curScore = Math.ceil(req.body.score);
+    const maxLength = 10;
+
     Score.find(function(err, scores) {
         if (err) {
             res.send('err');
         } else {
-            Score.remove({}).then(function() {
-                if(scores.length != 0) {
-                    var flag = false;
-                    for(var i=0 ; i<scores.length ; ++i) {
+            Score.deleteMany({}).then(function() {
+                scores.forEach(ele => {
+                    ele.score = parseInt(ele.score);
+                });
+                if (scores.length === 0) {
+                    res.statusCode = 201;
+                    scores = [{
+                        _id  : 1,
+                        name : curName,
+                        score: curScore
+                    }];
+                } else if (scores.length < maxLength) {
+                    res.statusCode = 201;
+                    if (scores[scores.length - 1].score > curScore) {
+                        scores.push({
+                            _id  : scores.length,
+                            name : curName,
+                            score: curScore
+                        });
+                    } else {
+                        for(let i=0 ; i<scores.length ; ++i) {
+                            if(curScore >= scores[i].score) {
+                                scores.splice(i, 0, {
+                                    _id  : i+1,
+                                    name : curName,
+                                    score: curScore
+                                });
+                                break;
+                            }
+                        }
+                    }
+                } else {
+                    for(let i=0 ; i<scores.length ; ++i) {
                         if(curScore >= scores[i].score) {
                             scores.splice(i, 0, {
                                 _id  : i+1,
                                 name : curName,
                                 score: curScore
                             });
-                            flag = true;
                             break;
                         }
                     }
-                    if(!flag) {
-                        scores.push({
-                            _id  : i+1,
-                            name: curName,
-                            score: curScore
-                        });
-                        while(scores.length > 10)
-                            scores.pop();
-                    }
+                    while(scores.length > maxLength)
+                        scores.pop();
                 }
-                else {
-                    scores.push({
-                        _id  : 1,
-                        name: curName,
-                        score: curScore
-                    });
-                }
-            
+
                 for(i=0 ; i<scores.length ; ++i)
                     scores[i]._id = i+1;
 
@@ -97,8 +116,19 @@ function setScores(req, res) {
 }
 
 app.get("/getScores/", getScores);
-app.post("/getScores/", getScores);
-app.post("/setScores/", jsonParser, setScores);
+app.put("/setScores/", jsonParser, setScores);
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, PUT');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Server started at port: ${PORT}`);
